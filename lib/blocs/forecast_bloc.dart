b0e176d59c321e2eba063948cc0b3f5ec612ed52
Ayham Orfali <orfali.ayham@gmail.com>
2020-11-29 23:16:41 +0200

    Added equatable to states

--- lib/blocs/forecast_bloc.dart
+++ lib/blocs/forecast_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:maidscc_waether/models/forecast.dart';
 import 'package:maidscc_waether/repositories/forecast_repository.dart';
 import 'package:meta/meta.dart';
--- lib/blocs/forecast_state.dart
+++ lib/blocs/forecast_state.dart
@@ -1,18 +1,30 @@
 part of 'forecast_bloc.dart';
 
 @immutable
-abstract class ForecastState {}
+abstract class ForecastState extends Equatable {}
 
-class ForecastLoading extends ForecastState {}
+class ForecastLoading extends ForecastState {
+  @override
+  // TODO: implement props
+  List<Object> get props => ['ForecastLoading'];
+}
 
 class ForecastLoaded extends ForecastState {
   final List<Forecast> forecasts;
 
   ForecastLoaded(this.forecasts);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => ['ForecastLoaded'];
 }
 
 class ForecastNotLoaded extends ForecastState {
   final String errorMsg;
 
   ForecastNotLoaded(this.errorMsg);
+
+  @override
+  // TODO: implement props
+  List<Object> get props => ['ForecastNotLoaded'];
 }
