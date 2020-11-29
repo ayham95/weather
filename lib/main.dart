6eb03cab947efe490c8d51619d123a7c985f35fc
Ayham Orfali <orfali.ayham@gmail.com>
2020-11-29 23:05:36 +0200

    Fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart';
-import 'package:maidscc_waether/forecast_api_client.dart';
 import 'package:maidscc_waether/repositories/forecast_repository.dart';
 
 import 'app.dart';
-import 'blocs/forecast_bloc.dart';
-import 'display/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- test/forecast_api_test.dart
+++ test/forecast_api_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:maidscc_waether/forecast_api_client.dart';
