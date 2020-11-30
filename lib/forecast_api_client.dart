1775e6261e9ddf7c50a30b00237e725da241993b
Ayham Orfali <orfali.ayham@gmail.com>
2020-11-30 12:41:16 +0200

    Added metrics units

--- lib/forecast_api_client.dart
+++ lib/forecast_api_client.dart
@@ -6,13 +6,16 @@ class ForecastAPIClient {
   final Client _client;
   static const String _base_url = 'api.openweathermap.org';
 
-  ForecastAPIClient({Client client})
-      : _client = client ?? Client();
+  ForecastAPIClient({Client client}) : _client = client ?? Client();
 
   Future<List<Forecast>> getData() async {
     try {
       // TODO Remove app secret ( you can use --dart-define ) but still won't work with tests...
-      final request = Uri.https(_base_url, '/data/2.5/forecast', {'id': '292223', 'appid' : '03297d312f41aeb574c6e709d61e33a7'});
+      final request = Uri.https(_base_url, '/data/2.5/forecast', {
+        'id': '292223',
+        'appid': '03297d312f41aeb574c6e709d61e33a7',
+        'units': 'metrics',
+      });
       final response = await _client.get(request);
       return ForecastList.fromJson(response.body).list.toList();
     } catch (e) {
